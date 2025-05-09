65c08ec5da6b4c09c45489ae19e9b72f4da9f1ee
Usman <119792239+UsmanA07@users.noreply.github.com>
2025-05-09 11:57:30 +0300

    Update post_repositories.py

--- src/apps/post/repositories/post_repositories.py
+++ src/apps/post/repositories/post_repositories.py
@@ -1,42 +1,47 @@
-from apps.post.dto.post_dto import PostListDTO, PostCreateDTO, PostDetailDTO, PostUpdateDTO
+from apps.post.dto.post_dto import (
+    PostListDTO,
+    PostCreateDTO,
+    PostDetailDTO,
+    PostUpdateDTO,
+)
 from apps.post.models import Post
 from abc import ABC, abstractmethod
 
 
 class PostRepository(ABC):
     @abstractmethod
-    def get_by_id(self, post_id: int):
+    def get_by_id(self, post_id: int) -> PostDetailDTO:
         pass
 
     @abstractmethod
-    def get_all(self):
+    def get_all(self) -> list[PostListDTO]:
         pass
 
     @abstractmethod
-    def post_create(self, post_dto: PostCreateDTO):
+    def post_create(self, post_dto: PostCreateDTO) -> PostCreateDTO:
         pass
 
     @abstractmethod
-    def update_by_id(self, post_id: int, post_dto: PostUpdateDTO):
+    def update_by_id(
+        self, post_id: int, post_dto: PostUpdateDTO
+    ) -> PostUpdateDTO | None:
         pass
 
     @abstractmethod
-    def delete_by_id(self, post_id: int):
+    def delete_by_id(self, post_id: int) -> bool:
         pass
 
 
 class DjangoPostRepository(PostRepository):
-
     def update_by_id(self, post_id: int, post_dto):
         try:
             post = Post.objects.get(id=post_id)
-            if post_dto.title is not None: post.title = post_dto.title
-            if post_dto.text is not None: post.text = post_dto.text
+            if post_dto.title is not None:
+                post.title = post_dto.title
+            if post_dto.text is not None:
+                post.text = post_dto.text
             post.save()
-            return PostUpdateDTO(
-                title=post.title,
-                text=post.text
-            )
+            return PostUpdateDTO(title=post.title, text=post.text)
         except Post.DoesNotExist:
             return None
 
@@ -50,16 +55,10 @@ class DjangoPostRepository(PostRepository):
 
     def post_create(self, post_dto: PostCreateDTO):
         post = Post.objects.create(
-            user=post_dto.user,
-            title=post_dto.title,
-            text=post_dto.text
+            user=post_dto.user, title=post_dto.title, text=post_dto.text
         )
 
-        return PostCreateDTO(
-            title=post.title,
-            text=post.text,
-            user=post.user
-        )
+        return PostCreateDTO(title=post.title, text=post.text, user=post.user)
 
     def get_by_id(self, post_id: int):
         post = Post.objects.get(id=post_id)
@@ -69,7 +68,7 @@ class DjangoPostRepository(PostRepository):
             user=post.user,
             title=post.title,
             text=post.text,
-            published=post.published
+            published=post.published,
         )
 
     def get_all(self):
@@ -80,7 +79,7 @@ class DjangoPostRepository(PostRepository):
                 user=post.user,
                 title=post.title,
                 text=post.text,
-                published=post.published
+                published=post.published,
             )
             for post in posts
         ]
